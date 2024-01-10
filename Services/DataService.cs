3dff56b1ab87af07e9f26aefcc3fb51f9856143f
Brent Cleary <brent.cleary@gmail.com>
2024-01-10 05:38:47 -0800

    SeedRolesAsync Method completed

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Build.Construction;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Construction;
 using TheBlogProject.Data;
+using TheBlogProject.Enums;
 
 namespace TheBlogProject.Services
 {
@@ -7,10 +9,12 @@ namespace TheBlogProject.Services
     {
 
         private readonly ApplicationDbContext _dbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public DataService(ApplicationDbContext dbContext)
+        public DataService(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager)
         {
             _dbContext = dbContext;
+            _roleManager = roleManager;
         }
 
         
@@ -28,12 +32,17 @@ namespace TheBlogProject.Services
         private async Task SeedRolesAsync()
         {
             // if there are already Roles in the System, do nothing
-            if (_dbContext.Roles.Any() == null)
+            if (_dbContext.Roles.Any())
             {
                 return;
             }
 
             // Otherwise we want to create a few Roles
+            foreach(var role in Enum.GetNames(typeof(BlogRole)))
+            {
+                // Use the Role Manager to create Roles
+                await _roleManager.CreateAsync(new IdentityRole(role));
+            }
         }
 
         private async Task SeedUsersAsync()
