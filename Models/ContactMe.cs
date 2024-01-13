6b8e391d35ff1c33661bd9b9a04976f1279a6f17
Brent Cleary <brent.cleary@gmail.com>
2024-01-13 10:28:36 -0800

    Updated Contact Model and Beginning ImageService

--- Models/ContactMe.cs
+++ Models/ContactMe.cs
@@ -14,7 +14,7 @@ namespace TheBlogProject.Models
         public string Email { get; set; }
 
         [Phone]
-        public int Phone { get; set; }
+        public string Phone { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = " The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
