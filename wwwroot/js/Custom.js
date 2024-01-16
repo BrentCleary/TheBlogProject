039b2679a26e68314bcaf15bd2d5f3056001b61e
Brent Cleary <brent.cleary@gmail.com>
2024-01-16 05:53:40 -0800

    Sweet Alerts Added: Not showing on error yet

--- wwwroot/js/Custom.js
+++ wwwroot/js/Custom.js
@@ -4,10 +4,22 @@ function AddTag() {
     // Get Reference to the TagEntry input element
     var tagEntry = document.getElementById("TagEntry");
 
-    // Create a new Select Option
-    let newOption = new Option(tagEntry.value, tagEntry.value);
-
-    document.getElementById("TagList").options[index++] = newOption;
+    // Lets use the new search function to help detect an error state
+    let searchResult = search(tagEntry.value);
+    if (searchResult != null) {
+        // Trigger sweet alert for whatever condition is contained in the search results var
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmationButtonText: 'Cool'
+        })
+    }
+    else {
+        // Create a new Select Option
+        let newOption = new Option(tagEntry.value, tagEntry.value);
+        document.getElementById("TagList").options[index++] = newOption;
+    }
 
     // Clear out the TagEntry control
     tagEntry.value = "";
@@ -29,10 +41,44 @@ function DeleteTag() {
             index--;
         }
     }
-
-
 }
 
 $("form").on("submit", function () {
     $("#TagList option").prop("selected", "selected");
 })
+
+// Look for the tagValues variable to see if it has data
+if( tagValues != '') {
+    let tagArray = tagValues.split(",");
+    for (let loop = 0; loop < tagArray.length; loop++) {
+        // Load up or Replace the options that we have
+        ReplaceTag(tagArray[loop], loop);
+        index++;
+    }
+}
+
+function ReplaceTag(tag, index) {
+    let newOption = new Option(tag, tag);
+    document.getElementById("TagList").options[index] = newOption;
+}
+
+
+// The Search function will detect either an empty of a duplicate Tag
+// and return and error string if an error is detected
+
+function search(str) {
+    if (str === "") {
+        return "Empty Tags are not permitted";
+    }
+
+    var tagsEl = document.getElementById("TagList");
+
+    if (tagsEl) {
+        let options = tagsEl.options;
+        for (let index = 0; index < options.length; i++) {
+            if (options[index].value == str) {
+                return `The Tag #${str} was detected as a duplicate and not permitted.`;
+            }
+        }
+    }
+}
